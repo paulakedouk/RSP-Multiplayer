6379a1447f65754503e4fb76a73c747001f61388
paulakedouk <paulakedouk@gmail.com>
2018-02-03 20:56:43 -0800

    Connected with database

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,60 @@
-// Two players
-// Each one can login
-// Player one choses and change to player two
-// Player two choses and show who wins
-// Resets and given points
-// If user close that tab, desconect and remove data from database
+//  Firebase Database Section
+var config = {
+  apiKey: 'AIzaSyDKvdgS0aaKWGmtKBAVEycngq1zbCPpMSE',
+  authDomain: 'rps-multiplayer-b342f.firebaseapp.com',
+  databaseURL: 'https://rps-multiplayer-b342f.firebaseio.com',
+  projectId: 'rps-multiplayer-b342f',
+  storageBucket: '',
+  messagingSenderId: '1042505507601'
+};
+firebase.initializeApp(config);
 
-$(document).on('ready', function() {
-  var config = {
-    apiKey: 'AIzaSyDKvdgS0aaKWGmtKBAVEycngq1zbCPpMSE',
-    authDomain: 'rps-multiplayer-b342f.firebaseapp.com',
-    databaseURL: 'https://rps-multiplayer-b342f.firebaseio.com',
-    projectId: 'rps-multiplayer-b342f',
-    storageBucket: '',
-    messagingSenderId: '1042505507601'
-  };
-  firebase.initializeApp(config);
+// Get a reference to the database service
+var database = firebase.database();
 
-  var database = firebase.database();
+database.ref('/players').on(
+  'value',
+  function(snapshot) {
+    console.log(snapshot);
+  },
+  function(errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+  }
+);
 
-  var player;
-  var opponent;
+$('#add-name').on('click', function(event) {
+  event.preventDefault();
+
+  // Get player name
+  var playerName = $('#name-input')
+    .val()
+    .trim();
+
+  var greeting = $('.greeting');
+  greeting.empty();
+
+  // Show greeting
+  var hi = $('<h3>').text(playerName);
+
+  $('.player-name')
+    .append(hi)
+    .append($('<h4>'));
+
+  // Save the new price in Firebase
+  database.ref('/players').set({
+    name: playerName
+  });
+
+  console.log(playerName);
 });
+
+// database.ref('/players').on('value', function(snapshot) {
+//   if (snapshot.child('player').exists()) {
+//     console.log('player exist');
+
+//     player = snapshot.val().player;
+//     playerName = name;
+//   } else {
+//     console.log("player doesn't exist");
+//   }
+// });
