2a40f2fd70c83c624e5a0d851856c3ab19e41256
paulakedouk <paulakedouk@gmail.com>
2018-02-04 16:09:50 -0800

    Show player choice on the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
+//  Firebase Database Section
+var config = {
+  apiKey: 'AIzaSyDKvdgS0aaKWGmtKBAVEycngq1zbCPpMSE',
+  authDomain: 'rps-multiplayer-b342f.firebaseapp.com',
+  databaseURL: 'https://rps-multiplayer-b342f.firebaseio.com',
+  projectId: 'rps-multiplayer-b342f',
+  storageBucket: '',
+  messagingSenderId: '1042505507601'
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
 var logic = {
   rock: {
     rock: 'tie',
@@ -20,51 +34,19 @@ var initialNum = 0;
 
 var player1 = {
   name: '',
-  selected: '',
+  choice: '',
   win: initialNum,
   lose: initialNum
 };
 
 var player2 = {
   name: '',
-  selected: '',
+  choice: '',
   win: initialNum,
   lose: initialNum
 };
 
-//  Firebase Database Section
-var config = {
-  apiKey: 'AIzaSyDKvdgS0aaKWGmtKBAVEycngq1zbCPpMSE',
-  authDomain: 'rps-multiplayer-b342f.firebaseapp.com',
-  databaseURL: 'https://rps-multiplayer-b342f.firebaseio.com',
-  projectId: 'rps-multiplayer-b342f',
-  storageBucket: '',
-  messagingSenderId: '1042505507601'
-};
-firebase.initializeApp(config);
-
-// Get a reference to the database service
-var database = firebase.database();
-
-database.ref('/players/').on(
-  'value',
-  function(snapshot) {
-    // if (snapshot.child('player1').exists()) {
-    //   console.log('Player exists');
-    // } else {
-    //   console.log('Player does NOT exist');
-    // }
-    // if (snapshot.child('player2').exists()) {
-    //   console.log('Opponent exists');
-    // } else {
-    //   console.log('Opponent does NOT exist');
-    // }
-  },
-  function(errorObject) {
-    console.log('The read failed: ' + errorObject.code);
-  }
-);
-
+// Listen to the click of the player1 form
 $('#player1-submit').on('click', function(event) {
   event.preventDefault();
 
@@ -73,7 +55,7 @@ $('#player1-submit').on('click', function(event) {
     .val()
     .trim();
 
-  // console.log(playerName);
+  var player1Choice;
 
   $('#player1-input').val('');
   player1.name = playerName;
@@ -84,6 +66,7 @@ $('#player1-submit').on('click', function(event) {
   database.ref('/players/player1').push({
     name: player1.name,
     win: player1.win,
+    choice: player1.choice,
     lose: player1.lose,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
@@ -91,6 +74,7 @@ $('#player1-submit').on('click', function(event) {
   logged();
 });
 
+// Listen to the click of the player2 form
 $('#player2-submit').on('click', function(event) {
   event.preventDefault();
 
@@ -98,8 +82,6 @@ $('#player2-submit').on('click', function(event) {
   var playerName = $('#player2-input')
     .val()
     .trim();
-
-  // console.log(playerName);
 
   $('#player1-input').val('');
   player2.name = playerName;
@@ -109,25 +91,30 @@ $('#player2-submit').on('click', function(event) {
   database.ref('/players/player2').push({
     name: player2.name,
     win: player2.win,
+    choice: player2.choice,
     lose: player2.lose,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 });
 
+// when the user is logged in, show a message to the other player
 function logged() {
-  //   $('.greeting-player1').hide();
-  //   $('.greeting-player2').hide();
+  // Check if the player name exist
   if (player1.name !== '') {
+    // Then hide the form
     $('.greeting-player1').hide();
     $('.form-player2').hide();
 
+    // Add a text to wait player2
     $('.player2-waiting')
       .text('Waiting for another player!')
       .addClass('waiting');
   } else if (player2.name !== '') {
+    // Hide the form
     $('.greeting-player2').hide();
     $('.form-player1').hide();
 
+    // Add a text to wait player1
     $('.player1-waiting')
       .text('Waiting for another player!')
       .addClass('waiting');
@@ -138,52 +125,39 @@ function logged() {
 }
 
 database.ref('/players/player1').on('child_added', function(snapshot) {
-  console.log(snapshot.val().name);
-  console.log(snapshot.val());
-
-  $('.player1-name').text(snapshot.val().name);
+  var player1Name = $('.player1-name').text(snapshot.val().name);
   $('.greeting-player1').empty();
+
+  $('.hands-player1').click(function() {
+    var value = $(this)[0];
+    var choice = $(value).attr('data-choice');
+    player1Chosen = $('.group-player1').html(value);
+
+    database.ref('/players/player1').set({
+      name: player1.name,
+      win: player1.win,
+      choice: choice,
+      lose: player1.lose
+    });
+  });
 });
 
 database.ref('/players/player2').on('child_added', function(snapshot) {
-  console.log(snapshot.val().name);
-  console.log(snapshot.val());
-
+  var player2Name = $('.player2-name').text(snapshot.val().name);
   $('.player2-name').text(snapshot.val().name);
   $('.greeting-player2').empty();
+
+  $('.hands-player2').click(function() {
+    var value = $(this)[0];
+    console.log(value);
+    var choice = $(value).attr('data-choice');
+    player2Chosen = $('.group-player2').html(value);
+
+    database.ref('/players/player2').set({
+      name: player2.name,
+      win: player2.win,
+      choice: choice,
+      lose: player2.lose
+    });
+  });
 });
-
-// var game = {
-//   playerUpdate: function() {
-//     database.ref('/players/player1').on('value', function(snapshot) {
-//       if (snapshot.val()) {
-//         player1.name = snapshot.val().name;
-//         logged();
-//       }
-//     });
-//     database.ref('/players/player2').on('value', function(snapshot) {
-//       if (snapshot.val()) {
-//         player2.name = snapshot.val().name;
-//         logged();
-//       }
-//     });
-//   },
-
-//   start: function() {
-//     database.ref('/players').on('value', function(snapshot) {
-//       if (
-//         snapshot
-//           .child('player1')
-//           .child('name')
-//           .val() &&
-//         snapshot
-//           .child('player2')
-//           .child('name')
-//           .val()
-//       ) {
-//         console.log('both logged');
-//         $('.player2-waiting').empty();
-//       }
-//     });
-//   }
-// };
